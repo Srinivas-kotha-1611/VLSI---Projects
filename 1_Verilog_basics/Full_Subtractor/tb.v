5a47127dc50d828cbdd9046d683c300d38be57b6
Srinivas-kotha-1611 <srinivaskotha174@yahoo.com>
2026-07-07 20:19:05 +0530

    Add testbench for full subtractor module

---
+++ 1_Verilog_basics/Full_Subtractor/tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ps
+
+module Subractor_tb;
+
+reg A, B, Bin;
+wire Diff, Bout;
+
+full_subtractor uut(
+    .A(A),
+    .B(B),
+    .Bin(Bin),
+    .Diff(Diff),
+    .Bout(Bout)
+);
+
+initial begin
+    A=0; B=0; Bin=0; #10;
+    A=0; B=0; Bin=1; #10;
+    A=0; B=1; Bin=0; #10;
+    A=0; B=1; Bin=1; #10;
+    A=1; B=0; Bin=0; #10;
+    A=1; B=0; Bin=1; #10;
+    A=1; B=1; Bin=0; #10;
+    A=1; B=1; Bin=1; #10;
+
+    $finish;
+end
+
+endmodule
